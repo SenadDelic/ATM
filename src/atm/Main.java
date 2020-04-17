5e7d28877a47a2e9b01e933643bb6dc63149b22b
SenadDelic <dellic.senad@gmail.com>
2020-04-17 20:49:09 +0200

    JMBG doesn't work :(

--- src/atm/Main.java
+++ src/atm/Main.java
@@ -1,8 +1,20 @@
 package atm;
 
+import account.Account;
+import account.User;
+import database.AccountManager;
+
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws SQLException {
+
+        User senad = new User("Senad", "Delic");
+        Account senadAcc = new Account(senad, 1, 300);
+
+        if (AccountManager.insert(senadAcc, senad))
+            System.out.println("New row with primary key " + senadAcc.getAccountId() + " are inserted!");
+
     }
 }
--- src/database/AccountManager.java
+++ src/database/AccountManager.java
@@ -1,4 +1,43 @@
 package database;
 
+import account.Account;
+import account.User;
+
+import java.sql.*;
+
 public class AccountManager {
+    private static Connection connection = ConnectionManager.getInstance().getConnection();
+
+
+    public static boolean insert(Account account, User user) throws SQLException {
+        String sql = "INSERT INTO Account (firstName, lastName, accountNumber, amount) "
+                + "VALUES(?, ?, ?, ?)";
+        ResultSet resultSet = null;
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getLastName());
+            preparedStatement.setInt(3, account.getAccountNumber());
+            preparedStatement.setDouble(4, account.getAmount());
+
+            int affected = preparedStatement.executeUpdate();
+
+            if (affected == 1) {
+                resultSet = preparedStatement.getGeneratedKeys();
+                resultSet.next();
+                int newRs = resultSet.getInt(1);
+                account.setAccountId(newRs);
+            } else {
+                System.err.println("No rows are affected :(");
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (resultSet != null) resultSet.close();
+        }
+        return true;
+    }
+
 }
