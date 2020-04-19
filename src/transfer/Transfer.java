bc8516f49a5b46c882cb9948f6b48285c9b1fd1c
SenadDelic <dellic.senad@gmail.com>
2020-04-19 20:22:29 +0200

    Done with Transfer

--- src/transfer/Transfer.java
+++ src/transfer/Transfer.java
@@ -8,12 +8,45 @@ public class Transfer {
     private double amountToTransfer;
 
     public Transfer() {
-        
+
     }
 
     public Transfer(Account sourceTarget, Account targetAccount, double amountToTransfer) {
         this.sourceTarget = sourceTarget;
         this.targetAccount = targetAccount;
         this.amountToTransfer = amountToTransfer;
+    }
+
+    public Account getSourceTarget() {
+        return sourceTarget;
+    }
+
+    public void setSourceTarget(Account sourceTarget) {
+        this.sourceTarget = sourceTarget;
+    }
+
+    public Account getTargetAccount() {
+        return targetAccount;
+    }
+
+    public void setTargetAccount(Account targetAccount) {
+        this.targetAccount = targetAccount;
+    }
+
+    public double getAmountToTransfer() {
+        return amountToTransfer;
+    }
+
+    public void setAmountToTransfer(double amountToTransfer) {
+        this.amountToTransfer = amountToTransfer;
+    }
+
+    @Override
+    public String toString() {
+        return "Transfer{" +
+                "sourceTarget=" + sourceTarget +
+                ", targetAccount=" + targetAccount +
+                ", amountToTransfer=" + amountToTransfer +
+                '}';
     }
 }
